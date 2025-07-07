423cda5c0fa1c1fa611867421448bb9371befd7e
Santiago Guzman <guzmansantiago833@gmail.com>
2025-07-07 00:34:39 -0500

    Day 10 🔥

--- Day 10 - Hold shift to Check Multiple Checkboxes/index.js
+++ Day 10 - Hold shift to Check Multiple Checkboxes/index.js
@@ -1,21 +1,21 @@
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]'); // Se seleccionan todos los elementos input de tipo checkbox
 
-let lastChecked;
+let lastChecked; // Se crea una variable sin valor para usar mas adelante en la que es seleccionada por el usuario
 
-function handleCheck(e){
-    let inBetween = false;
-    if (e.shiftKey && this.checked){
-        checkboxes.forEach(checkbox => {
-            if (checkbox === this || checkbox === lastChecked){
-                inBetween = !inBetween;
+function handleCheck(e){ // Se crea una funcion que escucha un evento
+    let inBetween = false; // Se crea una variable que se inicializa en false
+    if (e.shiftKey && this.checked){ // La condición valida si la checkbox actual en la iteración es igual a la que se acaba de hacer clic O a la última marcada
+        checkboxes.forEach(checkbox => { // Por cada checkbox hace una funcion que
+            if (checkbox === this || checkbox === lastChecked){ // Valida si la checkbox es igual a esa o si es igual a la ultima checkeada
+                inBetween = !inBetween; // Si es asi, cambia el estado false de inBetween
             };
             
-            if (inBetween){
-                checkbox.checked = true;
+            if (inBetween){ // Si inBetween es verdadera
+                checkbox.checked = true; // Cambia el estado de las checkbox a checked
             }
         })
     }
-    lastChecked = this;
+    lastChecked = this; // Y lastchecked se pasa a this en caso de que la funcion se dispare
 }
 
-checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck)); // Se crea un foreach para las checkboxes que por cada checkbox escucha un evento de click y ejecuta la funcion handleclick
