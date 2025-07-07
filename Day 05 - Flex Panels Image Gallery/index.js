4481594e91a7b361b486aa8fd4a53cf01741da44
Santiago Guzman <guzmansantiago833@gmail.com>
2025-07-07 16:55:18 -0500

    Day 05 🔥

---
+++ Day 05 - Flex Panels Image Gallery/index.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel'); // Obtenemos todos los elementos de clase panel 
+
+function open(){ // Creamos una funcion que 
+    this.classList.toggle('open'); // A los elementos obtenidos por el event listener, les agregara una clase open, y si ya la tiene, se la eliminara
+}
+
+function active(e){ // Luego, validamos otra funcion que recibe un evento
+    if(e.propertyName.includes('flex')){ // Del evento extraemos el propertyName, y si este incluye name:
+        this.classList.toggle('open-active'); // Agregamos la clase open-active, y si ya la tiene, la borramos
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', open)); // Por cada panel, agregamos un eventListener que valida el click y ejecuta open
+panels.forEach(panel => panel.addEventListener('transitionend', active)); // Por cada panel, agregamos un event listener que valida cuando terminan las transiciones que tienen flex
